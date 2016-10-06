c87a567d993b26a190b1095fe07fc211b70098a6
bryceluna <bryce.luna@gmail.com>
2016-10-05 19:56:14 -0600

    building multi-nomial NB model

--- NLP.py
+++ NLP.py
@@ -6,20 +6,24 @@ from sklearn.cross_validation import train_test_split
 def build_model(df):
     X = df['description']
     Y = df['acct_type']
-    X_train, X_test, y_train, y_test = train_test_split(x,Y, test_size=0.3, random_state=123)
-    model()
-    fit_transform()
-    return tf_mat, model
+    X_train, X_test, y_train, y_test = train_test_split(x,Y, test_size=0.2, random_state=123)
+    #might want to use CountVectorizer if you want to take into account doc length
+    vectorizer = TfidfVectorizer(stop_words='english')
+    tf_mat = vectorizer.fit_transform(X_train)
+    #should experiment with different alpha's
+    model = MultinomialNB(alpha=.01)
+    model.fit(tf_mat,y_train)
+    return vectorizer, model
 
 
 def main():
     df = pd.read_pickle('data/df_text')
-    tf_mat, model = build_model(df)
-    with open('','w') as v:
+    vectorizer, mnb_model = build_model(df)
+    with open('models/vectorizer.pkl','w') as v:
         pickle.dumps(,v)
-    with open('','w') as m:
-        pickle.dumps(,m)
+    with open('models/mnb_model.pkl','w') as m:
+        pickle.dumps(mnb_model,m)
 
-    
+
 if __name__ == '__main__':
     main()
