3f86c0194809a45e9c425cad5e52feb4f899dce5
bryce <bryce.luna@gmail.com>
2016-10-08 15:47:00 -0600

    fixed minor issues in NLP

--- NLP.py
+++ NLP.py
@@ -3,24 +3,28 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.cross_validation import train_test_split
 
+'''
+Notes:
+condider using CountVectorizer to take into account description length
+consider stemming and lemmatizing
+should experiment with different alpha's
+'''
+
 def build_model(df):
     X = df['description']
     Y = df['acct_type']
-    X_train, X_test, y_train, y_test = train_test_split(x,Y, test_size=0.2, random_state=123)
-    #might want to use CountVectorizer if you want to take into account doc length
+    X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.2, random_state=123)
     vectorizer = TfidfVectorizer(stop_words='english')
     tf_mat = vectorizer.fit_transform(X_train)
-    #should experiment with different alpha's
     model = MultinomialNB(alpha=.01)
     model.fit(tf_mat,y_train)
     return vectorizer, model
-
 
 def main():
     df = pd.read_pickle('data/df_text')
     vectorizer, mnb_model = build_model(df)
     with open('models/vectorizer.pkl','w') as v:
-        pickle.dumps(,v)
+        pickle.dumps(vectorizer,v)
     with open('models/mnb_model.pkl','w') as m:
         pickle.dumps(mnb_model,m)
 
