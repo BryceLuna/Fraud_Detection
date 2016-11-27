9d2cea553714cee584734ac8c1a2471b1808bd3c
bryce <bryce.luna@gmail.com>
2016-11-26 20:30:02 -0700

    added file to evaluate models

--- Models.py
+++ Models.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 import cPickle as pickle
 from scipy.stats import randint as sp_randint
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
 from sklearn.cross_validation import train_test_split
 from imblearn.over_sampling import SMOTE
 from sklearn.linear_model import LogisticRegression
